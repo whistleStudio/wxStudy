98f6df8b28ad177ec18e3ceee2d2e2f5736b57dd
whistleStudio <whistleStudio@126.com>
2020-04-27 17:31:53 +0800

    202004271731

--- pages/eventbind/eventbind.js
+++ pages/eventbind/eventbind.js
@@ -1,30 +1,11 @@
-// pages/databind/databind.js
+// pages/eventbind/eventbind.js
 Page({
+
   /**
    * 页面的初始数据
    */
   data: {
-    testText: "数据绑定",
-    testPrice: 2000,
-    testIsStar: false,
-    score: 90,
-    info: [
-      {title: "first", count: 99, _id: 0},
-      {title: "second", count: 88, _id: 1},
-      {title: "third", count: 77, _id: 2}
-    ],
-    switch: [
-      "a",
-      "b",
-      "c"
-    ]
-  },
 
-  _addSwitch: function() {
-    this.data.switch.unshift("new")
-    this.setData({
-      info: this.data.switch
-    })
   },
 
   /**
@@ -38,13 +19,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    setTimeout(()=>{
-      // this.data.info.unshift({title: "fourth", count: 66, _id: 3})
-      this.setData({
-        info: this.data.info.unshift({title: "fourth", count: 66, _id: 3})
-      })
-      console.log(this.data.info)
-    }, 3000)
+
   },
 
   /**
